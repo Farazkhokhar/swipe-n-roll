b3d6ad93674482a4a664e6b9ff2816affefb9571
adam <adam@anyro.se>
2010-07-26 14:26:29 +0000

    Added two new levels

--- src/se/anyro/snr/Level.java
+++ src/se/anyro/snr/Level.java
@@ -1,6 +1,7 @@
 package se.anyro.snr;
 
 import se.anyro.snr.bodies.Ball;
+import se.anyro.snr.bodies.Bridge;
 import se.anyro.snr.bodies.Goal;
 import se.anyro.snr.bodies.Hole;
 import se.anyro.snr.bodies.Wall;
@@ -27,11 +28,8 @@ public abstract class Level {
 			public void setup(GameThread gameThread) {
 				gameThread.add(new Wall(-2f, 5, 16, 2));
 		        gameThread.add(new Wall(2f, -5, 16, 2));
-		        
-		        gameThread.add(new Wall(-5, 0, 2, 3));
-		        gameThread.add(new Wall(5, 0, 2, 3));
-		        
-		        gameThread.add(new Wall(0, 0, 2, 2));
+		        		        
+		        gameThread.add(new Hole(0, 0));
 		        
 		        gameThread.add(new Goal(-8, 13));
 
@@ -44,11 +42,30 @@ public abstract class Level {
 				gameThread.add(new Wall(-2f, 5, 16, 2));
 		        
 		        gameThread.add(new Water(-6.5f, -3, 7, 3));
+		        gameThread.add(new Bridge(-1.5f, -3, 3, 3.5f));
 		        gameThread.add(new Water(5, -3, 10, 3));
 		        
 		        gameThread.add(new Goal(-8, 13));
 
 		        gameThread.add(new Ball(8, -13));
+			}
+		},
+		new Level() {
+			@Override
+			public void setup(GameThread gameThread) {
+				gameThread.add(new Hole(0, 13.5f));
+		        gameThread.add(new Hole(-8, 6.5f));
+		        gameThread.add(new Hole(-4, 3));
+		        gameThread.add(new Hole(8, -2.5f));
+		        gameThread.add(new Hole(-8, -5.5f));
+		        
+		        gameThread.add(new Wall(1.5f, 9, 17, 2));
+		        gameThread.add(new Wall(-1.5f, 0, 17, 2));
+		        gameThread.add(new Wall(-1.5f, -8, 17, 2));
+		        
+		        gameThread.add(new Goal(-8, 13));
+
+		        gameThread.add(new Ball(0, -13));
 			}
 		},
 		new Level() {
@@ -80,17 +97,35 @@ public abstract class Level {
 		new Level() {
 			@Override
 			public void setup(GameThread gameThread) {
-		        gameThread.add(new Hole(3.5f, -13));
-		        gameThread.add(new Hole(3.5f, -10.8f));
+
+		        gameThread.add(new Wall(0, 9.5f, 3, 11));
+		        gameThread.add(new Wall(-6, 2, 8, 3));
+		        gameThread.add(new Wall(6, 2, 8, 3));
+		        gameThread.add(new Wall(0, -5.5f, 3, 11));
+		        
+				// Lower right
+		        gameThread.add(new Hole(3.5f, -7.8f));
+		        gameThread.add(new Hole(3.5f, -10));
+		        gameThread.add(new Hole(5.7f, -10));
+		        gameThread.add(new Hole(7.9f, -10));
 		        
 		        gameThread.add(new Hole(7, -1.5f));
 		        gameThread.add(new Hole(7, -3.7f));
 		        gameThread.add(new Hole(7, -5.9f));
-		        gameThread.add(new Hole(7, -8.1f));
 		        
+		        // Upper right
+		        gameThread.add(new Hole(8, 7));
 		        gameThread.add(new Hole(8, 13));
-		        gameThread.add(new Hole(-8, 13));
+		        gameThread.add(new Hole(3.5f, 8));
+		        gameThread.add(new Hole(3.5f, 13));
 		        
+		        // Upper left
+		        gameThread.add(new Hole(-8, 7));
+		        gameThread.add(new Hole(-8, 13));
+		        gameThread.add(new Hole(-3.5f, 8));
+		        gameThread.add(new Hole(-3.5f, 13));
+		        
+		        // Lower left
 		        gameThread.add(new Hole(-7f, -1.5f));
 		        gameThread.add(new Hole(-7f, -3.7f));
 		        gameThread.add(new Hole(-7f, -5.9f));
@@ -99,32 +134,11 @@ public abstract class Level {
 		        gameThread.add(new Hole(-3.5f, -1.5f));
 		        gameThread.add(new Hole(-3.5f, -3.7f));
 		        
-		        gameThread.add(new Wall(0, 9.5f, 3, 11));
-		        gameThread.add(new Wall(-6, 2, 8, 3));
-		        gameThread.add(new Wall(6, 2, 8, 3));
-		        gameThread.add(new Wall(0, -5.5f, 3, 11));
+		        gameThread.add(new Hole(-4, -10.8f));
+		        gameThread.add(new Hole(-4, -13));
 		        
-		        gameThread.add(new Goal(-3.5f, -5.9f));
-
+		        gameThread.add(new Goal(8, -13));
 		        gameThread.add(new Ball(0, 0));
-			}
-		},
-		new Level() {
-			@Override
-			public void setup(GameThread gameThread) {
-				gameThread.add(new Hole(0, 13.5f));
-		        gameThread.add(new Hole(-8, 6.5f));
-		        gameThread.add(new Hole(-4, 3));
-		        gameThread.add(new Hole(8, -2.5f));
-		        gameThread.add(new Hole(-8, -5.5f));
-		        
-		        gameThread.add(new Wall(1.5f, 9, 17, 2));
-		        gameThread.add(new Wall(-1.5f, 0, 17, 2));
-		        gameThread.add(new Wall(-1.5f, -8, 17, 2));
-		        
-		        gameThread.add(new Goal(-8, 13));
-
-		        gameThread.add(new Ball(0, -13));
 			}
 		}
 	};
